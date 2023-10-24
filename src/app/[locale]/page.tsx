615a5c685a28625460e28463101b5f894f502901
donghhan <apensia914@gmail.com>
2023-10-24 13:10:59 +0700

    History responsive section added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import luxury from "public/luxury.jpg";
 import Header from "@/components/header/Header";
 import HeroSection from "@/components/home/hero/HeroSection";
 import LandOfSmileSection from "@/components/home/smiles/LandOfSmileSection";
+import HistorySection from "@/components/home/history/HistorySection";
 
 export default function Home() {
   const t = useTranslations("Home");
@@ -22,6 +23,7 @@ export default function Home() {
         title={t("smiles_title")}
         text={t("smiles_text")}
       />
+      <HistorySection title={t("history_title")} text={t("history_text")} />
     </main>
   );
 }
---
+++ src/components/home/history/HistorySection.tsx
@@ -0,0 +1,36 @@
+import { SectionProp } from "@/types/type";
+import "./style.HistorySection.scss";
+import buddha from "public/buddha-background.svg";
+import territory from "public/thailand-territory.png";
+import Image from "next/image";
+
+export default function HistorySection({
+  backgroundImage,
+  title,
+  text,
+}: SectionProp) {
+  return (
+    <section id="history">
+      <Image
+        src={buddha}
+        alt="History section background image"
+        fill
+        quality={100}
+        style={{ objectFit: "cover", filter: "brightness(50%)", zIndex: "-1" }}
+      />
+      <div className="container">
+        <div className="image-wrapper">
+          <Image
+            src={territory}
+            alt="Thailand territory image"
+            className="territory-image"
+          />
+        </div>
+        <div className="text-wrapper">
+          <h1 className="title">{title}</h1>
+          <p className="text">{text}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
